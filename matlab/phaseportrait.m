075a8b3adc50b966bafbc1d4aeda432c36a788e2
Toby Driscoll <driscoll@udel.edu>
2019-04-12 14:53:40 -0400

    Phase portrait generator

--- matlab/phaseportrait.m
+++ matlab/phaseportrait.m
@@ -1,40 +1,44 @@
 % Define your matrix here.
-A = [ -3 3;-6 3 ];
+A = [ -1 1;-2 -1 ];
 
 clf
 
 % This tries to make sensible choices for the initial conditions, based on
 % the stability
 lam = eig(A);
-tmax = 4/max(abs(lam));
+tmax = 10;
 numtheta = 20;
 alpha = max(real(lam));
+plot(0,0,'r.','markersize',18)
+
 if abs(alpha) < 1e-12
-    R = 0.33;
-    plot(0,0,'b.','markersize',18)
+    R = 0.75:.5:3.25;
+    theta = 0.123 + zeros(size(R));
 elseif alpha > 0
-    R = 0.15;    
-    plot(0,0,'r.','markersize',18)
+    theta = 0.123 + 2*pi*(0:15-1)/15;
+    R = 1.5 + 0.4*sin(theta);
 else 
-    R = 1;
-    plot(0,0,'k.','markersize',18)
+    theta = 0.123 + 2*pi*(0:15-1)/15;
+    R = 3.2 + 0.2*sin(theta);
 end
     
 % Plot the trajectories
 hold on
+axis([-4 4 -4 4]), axis square
+
 colr = get(gca,'colororder');
-t = linspace(0,tmax,300);
+t = linspace(-tmax,tmax,501);
 x = zeros(2,length(t));
-for theta = 2*pi*(0:numtheta-1)/numtheta
-    x0 = R*[ cos(theta); sin(theta) ];
-    for j = 1:length(t)
-        x(:,j) = expm(A*t(j))*x0;
+for i = 1:length(theta)
+    x0 = R(i)*[ cos(theta(i)); sin(theta(i)) ];
+    for k = 1:length(t)
+        x(:,k) = expm(A*t(k))*x0;
     end
-    plot(x(1,:),x(2,:),'linew',1.5,'color',colr(1,:))
+    arrow3(x(:,250)',x(:,251)','k',.5,1)
+    plot(x(1,:),x(2,:),'linew',1,'color',colr(1,:))
 end
 
 % Pretty it up
-axis([-1.5 1.5 -1.5 1.5]), axis square
 title(['eigenvalues ',num2str(lam(1)),',',num2str(lam(2))])
 xlabel('x_1')
 ylabel('x_2')
