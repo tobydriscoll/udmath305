529c025d4652f39958b0b80b3729870380e211af
Toby Driscoll <driscoll@udel.edu>
2020-04-14 19:53:57 -0400

    More second order updates

--- src/matlab/so_oscgain.m
+++ src/matlab/so_oscgain.m
@@ -1,17 +1,18 @@
 
       close all
       
-A = @(omega,Z) 1./(-omega.^2 + 2i*Z.*omega + 1 );
-fsurf( @(omega,Z) log10(abs(A(omega,Z))),[0 1.5 1e-3 1])
-view(-36,50), set(gca,'ydir','rev')
+A = @(omega,Z) 1./(-omega.^2 + 2i*Z.*omega + 1);
+log_g = @(omega,Z) log10(abs(A(omega,Z)));
+fsurf( log_g,[0 1.5 1e-3 1])
+set(gca,'ydir','rev')
 view(20,20)
 xlabel('driving frequency')
-ylabel('damping intensity')
+ylabel('damping coefficient')
 zlabel('log_{10}(gain)')
 hold on
 Z = linspace(1e-3,1,300);
 rhomax = real(sqrt(1-2*Z.^2));
-plot3(rhomax,Z,log10(abs(A(rhomax,Z))),'w','linew',3)
+plot3(rhomax,Z,log_g(rhomax,Z),'w','linew',2)
 					
       if length(get(0,'children')) > 0
         set(gcf,'paperpos',[0 0 5 2.75])
